9e78b8f82f3e95e74cd26fb1a21e24103b1c4d8d
smutallib97 <smutallib97@gmail.com>
2022-10-13 14:49:12 +0530

    Simple Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -1,7 +1,56 @@
 package com.bridgelabz;
 
 public class LinkedList<T> {
+    class Node <T>{
+        T data;
+        Node<T> next;
+        public Node(T data){
+            this.data = data;
+        }
+    }
+    Node<T> head;
+    Node<T> tail;
+    public void push(T data) {
+        Node<T> newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }else{
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+    public void show(){
+        if(head == null){
+            System.out.println("Linked list is Empty");
+        }
+        else{
+            Node<T> temp = head;
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Linked list program");
+        LinkedList<Integer> list1 = new LinkedList();
+        list1.push(70);
+        list1.push(30);
+        list1.push(56);
+        list1.show();
+
     }
 }
